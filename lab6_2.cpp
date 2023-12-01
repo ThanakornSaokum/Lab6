730e159b291fda60afbff5d3483ad90ed20c94da
Thanakorn <thanakorn0130@gmail.com>
2023-12-01 17:12:23 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,8 +1,41 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main(){
+double deg2rad(double x){
+    return M_PI*x/180.0;
+   
+}
+double rad2deg(double x){
+    return x/M_PI*180.0;
+}
+double findXComponent(double a1, double a2, double l1, double l2){
+    return cos(a1)*l1+cos(a2)*l2;
+}
+double findYComponent(double a1, double a2, double l1, double l2){
+    return sin(a1)*l1+sin(a2)*l2;
+}
+double pythagoras(double xcomp, double ycomp){
+    return sqrt(pow(xcomp,2)+pow(ycomp,2));
+}
+void showResult(double result_vec_length, double result_vec_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = " << result_vec_length << "\n";
+    cout << "Direction of the resultant vector (deg) = " << result_vec_direction << "\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
+
+int main()
+{
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
     cin >> l1;
@@ -21,4 +54,7 @@ int main(){
     result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
 
     showResult(result_vec_length,result_vec_direction);
+    
+
+    return 0;
 }
